43614fadb383a94d1d5db1bd3a84f28de7c98f36
shahrokh ziglari <shahrokh.ziglari@outlook.com>
2020-12-06 12:19:09 +0330

    Add project files.

---
+++ Test/Shared/Entities/Karmand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Shared.Entities
+{
+    public class Karmand
+    {
+        public string name { get; set; }
+        public DateTime lastOnline { get; set; }
+    }
+}
